9046cc3861a4091cb8f2403139eacace1e550685
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-08-07 13:28:46 +0300

    feat: add FillArray method

---
+++ FinalTask/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] Fillarray(string[] your_array)
+{
+    int length = your_array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine("Enter any characters");
+        your_array[i] = Console.ReadLine()!;
+        
+    }
+    return your_array;
+}
+
+
+Console.WriteLine("Enter array length ");
+int array_length = int.Parse(Console.ReadLine()!);
+string[] array = new string[array_length];
+string[] stringArray = Fillarray(array);
